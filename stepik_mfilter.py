0fd308cd0a380e492487a6c0612389b5e89f03ee
Vityarock <bvs@maxidom.ru>
2020-05-01 20:22:17 +0300

    new file from task class_iterator

---
+++ stepik_mfilter.py
@@ -0,0 +1,72 @@
+class multifilter:
+    def judge_half(pos, neg):
+        if pos >= neg:
+            # print("half")
+            return True# допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg)
+
+    def judge_any(pos, neg):
+        if pos >= 1:
+            # print("any")
+            return True# допускает элемент, если его допускает хотя бы одна функция (pos >= 1)
+
+    def judge_all(pos, neg):
+        if neg == 0:
+            # print("all")
+            return True# допускает элемент, если его допускают все функции (neg == 0)
+
+    def __init__(self, iterable, *funcs, judge=judge_any):
+        self.iterable = iterable# iterable - исходная последовательность
+        self.funcs = funcs# funcs - допускающие функции
+        self.judge = judge# judge - решающая функция
+        self.index = 0
+
+
+
+    def __iter__(self):
+        return self# возвращает итератор по результирующей последовательности
+        
+    def __next__(self):
+        
+        if self.index < len(self.iterable):
+            self.index += 1
+            self.pos = 0
+            self.neg = 0
+            self.result = None
+
+            for func in self.funcs:
+                if func(self.iterable[self.index - 1]):
+                    self.pos += 1
+                else:
+                    self.neg += 1
+                # print(self.pos, self.neg)
+            if self.judge(self.pos, self.neg):
+                self.result = self.iterable[self.index - 1]
+                print("Ok")
+            if self.result != None:
+                return self.result
+                
+
+ 
+        raise StopIteration
+
+
+def mul2(x):
+    return x % 2 == 0
+
+def mul3(x):
+    return x % 3 == 0
+
+def mul5(x):
+    return x % 5 == 0
+
+
+a = [i for i in range(0, 31)] # [0, 1, 2, ... , 30]
+
+print(list(multifilter(a, mul2, mul3, mul5))) 
+# [0, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30]
+
+print(list(multifilter(a, mul2, mul3, mul5, judge=multifilter.judge_half))) 
+# [0, 6, 10, 12, 15, 18, 20, 24, 30]
+
+print(list(multifilter(a, mul2, mul3, mul5, judge=multifilter.judge_all))) 
+# [0, 30]
